435c4bde2362b322c0c88f4408ea0bd548bb5f5e
Dmytro Khymytsia <dimahimitsya@gmail.com>
2025-10-08 00:29:47 +0200

    Adding USART
    - added definitons for USART
    - added initializations for USART registers

--- inc/registers.h
+++ inc/registers.h
@@ -6,10 +6,16 @@
 // RCC definiton
 #define RCC_BASE 0x40021000UL
 #define RCC_IOPENR (*(volatile uint32_t*)(RCC_BASE + 0x2C))
+#define RCC_APB2ENR (*(volatile uint32_t*)(RCC_BASE + 0x34))
 
 // GPIO definition
 #define GPIOA_BASE 0X50000000UL
 #define GPIOA_MODER (*(volatile uint32_t*)(GPIOA_BASE + 0x00))
 #define GPIOA_ODR   (*(volatile uint32_t*)(GPIOA_BASE + 0x14))
+#define GPIOx_AFRH  (*(volatile uint32_t*)(GPIOA_BASE + 0x24))
+
+// USART definiton
+#define USART_BASE 0X40013800UL
+#define USART_BRR   (*(volatile uint32_t*)(USART_BASE + 0x0C))
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,10 +3,21 @@
 int main(void) {
     // Open tackting
     RCC_IOPENR |= (1 << 0);
+    RCC_APB2ENR |= (1 << 14);
 
-    // PA0 output
+    // PA5 output
     GPIOA_MODER &= ~(3 << (2 * 5));
     GPIOA_MODER |= (1 << (2 * 5));
+
+    //PA9 alternate function
+    GPIOA_MODER &= ~(3 << (2 * 9));
+    GPIOA_MODER |= (2 << (2 * 9));
+    GPIOx_AFRH |= (1 << (1 * 4));
+
+    //PA10 alternate function
+    GPIOA_MODER &= ~(3 << (2 * 10));
+    GPIOA_MODER |= (2 << (2 * 10));
+    GPIOx_AFRH |= (1 << (2 * 4));
 
     while(1) {
         for(int i = 0; i < 100000; i++) {
