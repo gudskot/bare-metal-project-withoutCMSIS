bc726d8f63bd6b2e4e1a99893496e5f928527784
Dmytro Khymytsia <dimahimitsya@gmail.com>
2025-11-02 20:14:33 +0100

    Added UART interface

--- inc/registers.h
+++ inc/registers.h
@@ -12,10 +12,18 @@
 #define GPIOA_BASE 0X50000000UL
 #define GPIOA_MODER (*(volatile uint32_t*)(GPIOA_BASE + 0x00))
 #define GPIOA_ODR   (*(volatile uint32_t*)(GPIOA_BASE + 0x14))
-#define GPIOx_AFRH  (*(volatile uint32_t*)(GPIOA_BASE + 0x24))
+#define GPIOA_AFRH  (*(volatile uint32_t*)(GPIOA_BASE + 0x24))
 
 // USART definiton
 #define USART_BASE 0X40013800UL
+#define USART_CR1   (*(volatile uint32_t*)(USART_BASE + 0x00))
 #define USART_BRR   (*(volatile uint32_t*)(USART_BASE + 0x0C))
+#define USART_ISR   (*(volatile uint32_t*)(USART_BASE + 0x1C))
+#define USART_TDR   (*(volatile uint32_t*)(USART_BASE + 0x28))
+#define USART_RDR   (*(volatile uint32_t*)(USART_BASE + 0x24))
+
+//  USART mask
+#define USART_ISR_TXE (1 << 7)
+#define USART_ISR_RXNE (1 << 5)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
 #include <registers.h>
+#include <stdio.h>
+
+void uart_transmit(char c) {
+    while(!(USART_ISR & USART_ISR_TXE)) {}
+    USART_TDR = c;
+}
+
+char uart_recieve(void) {
+    while(!(USART_ISR & USART_ISR_RXNE)) {}
+    return USART_RDR;
+}
+
+void uart_send_string(const char *s) {
+    while(*s) {
+        uart_transmit(*s++);
+        GPIOA_ODR ^= (1 << 5);
+    }
+}
 
 int main(void) {
     // Open tackting
@@ -12,18 +30,26 @@ int main(void) {
     //PA9 alternate function
     GPIOA_MODER &= ~(3 << (2 * 9));
     GPIOA_MODER |= (2 << (2 * 9));
-    GPIOx_AFRH |= (1 << (1 * 4));
+    GPIOA_AFRH &= ~(4 << (1 * 4)); // reset bits
+    GPIOA_AFRH |= (4 << 4);   // function type AF4  
 
     //PA10 alternate function
     GPIOA_MODER &= ~(3 << (2 * 10));
     GPIOA_MODER |= (2 << (2 * 10));
-    GPIOx_AFRH |= (1 << (2 * 4));
+    GPIOA_AFRH &= ~(4 << (2 * 4));  // reset bits
+    GPIOA_AFRH |= (4 << 8);   // function type AF4
+
+    //  set baud rate
+    USART_BRR = 18; // (2.1Mhz / 115200)
+    
+    //  Enable USART
+    USART_CR1 = 0;  // reset all bits
+    USART_CR1 |= (1 << 2);  // enable transmit
+    USART_CR1 |= (1 << 3);  // enable recieve
+    USART_CR1 |= (1 << 0);  // enable USART
 
     while(1) {
-        for(int i = 0; i < 100000; i++) {
-            GPIOA_ODR ^= (1 << 5);
-            for(int i = 0; i < 100000; i++) {};
-            GPIOA_ODR ^= (0 << 5);
-        }
+        uart_send_string("Hello World\n");
+        for(int i = 0; i < 100000; i++);
     }
 }
