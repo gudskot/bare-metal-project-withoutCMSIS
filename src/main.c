a20cb6d63ce1f90b8ca3a374d37a0b8d5b8f0fcd
Dmytro Khymytsia <dimahimitsya@gmail.com>
2025-11-11 22:02:58 +0100

    Added command control

--- src/main.c
+++ src/main.c
@@ -1,55 +1,72 @@
 #include <registers.h>
+#include <gpio.h>
 #include <stdio.h>
 
 void uart_transmit(char c) {
-    while(!(USART_ISR & USART_ISR_TXE)) {}
+    while(!(USART_ISR & USART_ISR_TXE));
     USART_TDR = c;
 }
 
 char uart_recieve(void) {
-    while(!(USART_ISR & USART_ISR_RXNE)) {}
+    while(!(USART_ISR & USART_ISR_RXNE));
     return USART_RDR;
 }
 
 void uart_send_string(const char *s) {
-    while(*s) {
-        uart_transmit(*s++);
-        GPIOA_ODR ^= (1 << 5);
-    }
+    while(*s) uart_transmit(*s++);
+}
+
+void ledInfo(void) {
+    if((GPIOA_ODR & (1 << 5))) uart_send_string("LED PA5 is: HIGH\n");
+    else uart_send_string("LED PA5 is: LOW\n");
 }
 
 int main(void) {
+    char c;
+
     // Open tackting
     RCC_IOPENR |= (1 << 0);
     RCC_APB2ENR |= (1 << 14);
 
     // PA5 output
-    GPIOA_MODER &= ~(3 << (2 * 5));
-    GPIOA_MODER |= (1 << (2 * 5));
+    GPIOA_MODER &= ~GPIO_CLEAR(5);
+    GPIOA_MODER |= GPIO_MODE_OUTPUT(5);
 
     //PA9 alternate function
-    GPIOA_MODER &= ~(3 << (2 * 9));
-    GPIOA_MODER |= (2 << (2 * 9));
-    GPIOA_AFRH &= ~(4 << (1 * 4)); // reset bits
-    GPIOA_AFRH |= (4 << 4);   // function type AF4  
+    GPIOA_MODER &= ~GPIO_CLEAR(9);
+    GPIOA_MODER |= GPIOA_MODE_AF(9);
+    GPIOA_AFRH &= ~AF_CLEAR(9); // reset bits
+    GPIOA_AFRH |= AF_TYPE4(9);   // function type AF4  
 
     //PA10 alternate function
-    GPIOA_MODER &= ~(3 << (2 * 10));
-    GPIOA_MODER |= (2 << (2 * 10));
-    GPIOA_AFRH &= ~(4 << (2 * 4));  // reset bits
-    GPIOA_AFRH |= (4 << 8);   // function type AF4
+    GPIOA_MODER &= ~GPIO_CLEAR(10);
+    GPIOA_MODER |= GPIOA_MODE_AF(10);
+    GPIOA_AFRH &= ~AF_CLEAR(10);  // reset bits
+    GPIOA_AFRH |= AF_TYPE4(10);   // function type AF4
 
     //  set baud rate
     USART_BRR = 18; // (2.1Mhz / 115200)
     
     //  Enable USART
-    USART_CR1 = 0;  // reset all bits
-    USART_CR1 |= (1 << 2);  // enable transmit
-    USART_CR1 |= (1 << 3);  // enable recieve
-    USART_CR1 |= (1 << 0);  // enable USART
+    USART_CR1 = USART_RESET;  // reset all bits
+    USART_CR1 |= USART_ENABLE_TRANSMIT;  // enable transmit
+    USART_CR1 |= USART_ENABLE_RECIEVE;  // enable recieve
+    USART_CR1 |= USART_ENABLE;  // enable USART
 
     while(1) {
-        uart_send_string("Hello World\n");
-        for(int i = 0; i < 100000; i++);
+        uart_send_string("\nEnter number of operation:\n1 - LED info\n2 - Toggle LED\n");
+
+        c = uart_recieve();
+        switch(c) {
+        case '1':
+            ledInfo();
+            break;
+        case '2':
+            GPIOA_ODR ^= (1 << 5);
+            break;
+        default:
+            uart_send_string("Wrong option. Try again\n");
+            break;
+        }
     }
 }
